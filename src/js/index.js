3f289fffa30e270790f2a54880c77068716233c4
gzr529 <guseyn.zarbaliyev23@sitechhs.com>
2021-05-12 09:41:12 -0400

    v2
    -Shows Top 50 Anime of All Time

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
+const query = async function () {
+  const response = await fetch("https://api.jikan.moe/v3/top/anime/1/tv");
+  console.log(response);
+  const data = await response.json();
+  console.log(data.top);
+  
+  data.top.forEach((anime) => {
+  topRated.insertAdjacentHTML("beforeend", `<p class=anime-title>${anime.title}</p> <div class="anime-cover">
+     <img src="${anime.image_url}"
+     class="anime-cover"/>
+    </div`);
+});
+
+};
+query();
+
 
