be89fea6bcccc82db33d8728a84cd3edc2169235
gzr529 <guseyn.zarbaliyev23@sitechhs.com>
2021-05-26 05:48:04 -0400

    v3
    (STILL) Improving NavBar

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
-const query = async function () {
+const topAnimeAll = async function () {
   const response = await fetch("https://api.jikan.moe/v3/top/anime/1/tv");
-  console.log(response);
   const data = await response.json();
-  console.log(data.top);
 
   let topTen = data.top.splice(10, 50);
 
@@ -16,4 +14,28 @@ const query = async function () {
     );
   });
 };
-query();
+
+/* topAnimeAll(); */
+
+fetch;
+
+const search = async function () {
+  const response = await fetch(
+    `https://api.jikan.moe/v3/search/anime?q=${amime.name}`
+  );
+  const data = await response.json();
+
+  let topTen = data.top.splice(10, 50);
+
+  data.top.forEach((topTen) => {
+    topRated.insertAdjacentHTML(
+      "beforeend",
+      `<div class="placeholder"> <div class=bg> <p class=anime-title>${topTen.title}</p> <div class="anime-cover">
+     <img src="${topTen.image_url}"
+     class="anime-cover"/>
+    </div></div></div>`
+    );
+  });
+};
+
+search();
